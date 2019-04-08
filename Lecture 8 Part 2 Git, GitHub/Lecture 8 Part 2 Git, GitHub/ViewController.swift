9902f3bf1967b81f2842cdaad62b6225d5f3dcf3
Pavel-Pats <belmain1944@gmail.com>
2019-04-08 11:28:22 +0300

    added project;

---
+++ Lecture 8 Part 2 Git, GitHub/Lecture 8 Part 2 Git, GitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lecture 8 Part 2 Git, GitHub
+//
+//  Created by belmain on 08/04/2019.
+//  Copyright © 2019 belmain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
